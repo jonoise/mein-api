b1cc6978cd7c1763efbbcf3212b2c51af4d23e9d
Amilkar Munoz <yellowballpython@gmail.com>
2021-08-30 23:10:31 -0600

    settings file split

---
+++ core/settings/dev.py
@@ -0,0 +1,17 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'losjunior',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+CORS_ALLOW_ALL_ORIGINS = True
