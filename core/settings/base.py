57068fab777931416b6d5af0f3b40a2e809fda7e
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-01 20:58:21 -0600

    add cors sttings

--- core/settings/base.py
+++ core/settings/base.py
@@ -45,12 +45,10 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'drf_yasg',
     "corsheaders",
-
 ]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
