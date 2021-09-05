2789a3332a1d60ce26480b0a011efd579cd0ac5d
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-04 19:54:42 -0600

    add created field

--- apps/table_instances/models.py
+++ apps/table_instances/models.py
@@ -7,3 +7,10 @@ class TableInstance(models.Model):
         Table, on_delete=models.CASCADE, related_name="instances")
     uuid = models.CharField(max_length=255)
     is_valid = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self) -> str:
+        return f'Instance {self.id} - {self.table}'
