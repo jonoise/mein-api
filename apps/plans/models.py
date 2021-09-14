60531059d10f0d3831bca1f777058bc2cfaf9583
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-14 01:40:36 -0600

    add extra fields to plan

--- apps/plans/models.py
+++ apps/plans/models.py
@@ -13,8 +13,12 @@ class Plan(models.Model):
         Restaurant, on_delete=models.CASCADE, related_name="plans")
     type_of = models.CharField(max_length=20, default="free")
     pricing = models.PositiveIntegerField(default=0)
-    paid_until = models.DateField()
+    max_orders = models.PositiveIntegerField(default=100)
+    max_tables = models.PositiveIntegerField(default=3)
+    paid_until = models.DateField(null=True, blank=True)
 
     def is_active(self):
         today = datetime.date.today()
+        if not self.paid_until:
+            return False
         return self.paid_until > today
