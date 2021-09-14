a300f66da5feabccd9f52861cea2a2409fd1d895
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-14 01:40:24 -0600

    add signal to create Plan

--- apps/restaurants/signals.py
+++ apps/restaurants/signals.py
@@ -1,9 +1,20 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Restaurant
+from apps.plans.models import Plan
 
 
 # POST INIT SLUG
 @receiver(pre_save, sender=Restaurant)
 def createFreshSlug(sender, instance, **kwargs):
     instance.create_slug()
+
+
+# CREATE PLAN
+@receiver(post_save, sender=Restaurant)
+def createRestaurantPlan(sender, instance, created, **kwargs):
+    if created:
+        Plan.objects.create(
+            owner=instance.owner,
+            restaurant=instance,
+        )
