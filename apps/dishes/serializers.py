7fff6a94146c1cf8043ee2fec64457c0dde2eb9a
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-14 00:49:06 -0600

    specifics serializers

--- apps/dishes/serializers.py
+++ apps/dishes/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from .models import Dish
+from .models import Dish, OptionField, Specific
 
 
 class DishSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dish
         fields = "__all__"
+
+
+class SpecificOptionFieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OptionField
+        fields = "__all__"
+
+
+class DishSpecificsSerializer(serializers.ModelSerializer):
+    _options = SpecificOptionFieldSerializer(many=True)
+
+    class Meta:
+        model = Specific
+        fields = "__all__"
+        extra_fields = ["_options"]
