50fec009409ee304ebcc7ea5d05b13a0748145ab
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-18 13:03:57 -0600

    add signal

--- apps/restaurants/signals.py
+++ apps/restaurants/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Restaurant
 from apps.plans.models import Plan
+from apps.kitchens.models import Kitchen
 
 
 # POST INIT SLUG
@@ -16,5 +17,8 @@ def createRestaurantPlan(sender, instance, created, **kwargs):
     if created:
         Plan.objects.create(
             owner=instance.owner,
+            restaurant=instance,
+        )
+        Kitchen.objects.create(
             restaurant=instance,
         )
