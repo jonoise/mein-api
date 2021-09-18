e125b525eba643cbdca95986d5fceea7a296e3bb
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-18 13:03:42 -0600

    add counting serializer

--- apps/restaurants/serializers.py
+++ apps/restaurants/serializers.py
@@ -3,10 +3,20 @@ from .models import Restaurant
 from apps.plans.serializers import PlanSerializer
 
 
+class CountingSerializer(serializers.Serializer):
+    menus = serializers.IntegerField()
+    tables = serializers.IntegerField()
+    dishes = serializers.IntegerField()
+    orders = serializers.IntegerField()
+    total_last_month = serializers.IntegerField()
+    total_last_week = serializers.IntegerField()
+
+
 class RestaurantSerializer(serializers.ModelSerializer):
     _plan = PlanSerializer()
+    _counting = CountingSerializer()
 
     class Meta:
         model = Restaurant
         fields = "__all__"
-        extra_fields = "_plan"
+        extra_fields = ["_plan", "_counting"]
