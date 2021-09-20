e1be4e53ee14a21b63582c9a60592cdd04148f78
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-20 13:30:33 -0600

    add checkedDish and kitchenDish to admin

--- apps/checks/admin.py
+++ apps/checks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Check
+from .models import Check, CheckedDish
 
 admin.site.register(Check)
+admin.site.register(CheckedDish)
--- apps/kitchens/admin.py
+++ apps/kitchens/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Kitchen
+from .models import Kitchen, KitchenDish
 # Register your models here.
 
 admin.site.register(Kitchen)
+admin.site.register(KitchenDish)
